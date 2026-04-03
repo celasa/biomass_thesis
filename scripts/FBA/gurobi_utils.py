99a15a62303bde5ebb48e7f1575ad74b68266f81
celasa <celina.h-a@hotmail.com>
2026-04-03 18:53:09 +0200

    mu fix

--- scripts/FBA/gurobi_utils.py
+++ scripts/FBA/gurobi_utils.py
@@ -7,7 +7,7 @@ def run_substrate_gurobi(model, substrate):
     rxn = model.reactions.get_by_id(substrate)
     rxn.lower_bound = -1000
 
-    solution = algorithms.binary_search(model, min_mu=0.1, max_mu=2.0, verbose=True, solver="gurobi", mu_accuracy=1e-6)
+    solution = algorithms.binary_search(model, min_mu=0.1, max_mu=1.5, verbose=True, solver="gurobi", mu_accuracy=1e-6)
 
     return {"fluxes": solution.x_dict, 
             "shadow": solution.y_dict,
