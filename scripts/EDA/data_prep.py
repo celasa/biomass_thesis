8ca649cdca6e1c443767c7b315a9654c3a127f51
celasa <188046832+celasa@users.noreply.github.com>
2026-05-06 10:51:33 +0200

    Update data_prep.py

--- scripts/EDA/data_prep.py
+++ scripts/EDA/data_prep.py
@@ -1,7 +1,14 @@
 # Cleaning up construction of datasets:
 
 ## 1. Biomass-producing reactions datasets
+def get_samples(path):
+    # make one large list before using merge_samples when combining all 
+    """"Returns a list of DataFrames, each df is the entire flux vector/shaadow price vector of a sample"""
+    folder = Path(path)
+    files = sorted(folder.glob("*.pkl"))
+    dfs = [pd.read_pickle(pkl) for pkl in files]
 
+    return dfs
 
 
 def fix_col_names(samples_df, substrate):   
