027a8e9011da355c31c3d525c36764aa1edd0b2b
celasa <celina.h-a@hotmail.com>
2026-06-23 18:00:35 +0200

    amino_acids

--- scripts/EDA/data_prep.py
+++ scripts/EDA/data_prep.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
 # Cleaning up construction of datasets:
 
 ## 1. Biomass-producing reactions datasets
